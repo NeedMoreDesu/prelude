77a6714d32ac5a2a28a926b6f992f62f3b920fba
Oleksii Horishnii <oleksii.horishnii@gmail.com>
2019-02-08 20:11:19 +0200

    rewrite so positions don't insert symbols

--- personal/provide/save-position.el
+++ personal/provide/save-position.el
@@ -1,4 +1,5 @@
 ;;; -*- lexical-binding: t -*-
+(require 'color)
 
 (defun save-position-plist-merge (&rest plists)
   (if plists
@@ -11,21 +12,44 @@
       result)
     nil))
 
-(defun save-position-create-new-region (name fg-color bg-color)
+(defun save-position-create-new-region (name bg-color)
   (list
     :name name
-    :fg-color fg-color
     :bg-color bg-color))
 
 (defun save-position-save-current-region (arg)
-  (let ( (begin (copy-marker (point)))
-         (end (copy-marker (if (use-region-p) (mark) (point)))) )
+  (let* ( (begin (copy-marker (point)))
+          (end (copy-marker (if (use-region-p) (mark) (point))))
+          (begin<end (< (marker-position begin) (marker-position end)))
+          (begin-marker-overlay (make-overlay (- begin 1) begin))
+          (end-marker-overlay (make-overlay end (+ end 1)))
+          (begin-overlay (if begin<end begin-marker-overlay end-marker-overlay))
+          (end-overlay (if begin<end end-marker-overlay begin-marker-overlay))
+          (begin-overlay-change-fn (lambda (ov done &rest args)
+                                     (if done
+                                       (let ( (inhibit-modification-hooks t)
+                                              (pos (marker-position begin)))
+                                         (move-overlay ov
+                                           (- pos 1)
+                                           pos)))))
+          (end-overlay-change-fn (lambda (ov done &rest args)
+                                   (if done
+                                     (let ( (inhibit-modification-hooks t)
+                                            (pos (marker-position end)))
+                                       (move-overlay ov
+                                         pos
+                                         (+ pos 1)))))) )
+    (overlay-put begin-marker-overlay 'insert-in-front-hooks (list begin-overlay-change-fn))
+    (overlay-put end-marker-overlay 'insert-in-front-hooks (list end-overlay-change-fn))
+    (overlay-put begin-marker-overlay 'modification-hooks (list begin-overlay-change-fn))
+    (overlay-put end-marker-overlay 'modification-hooks (list end-overlay-change-fn))
     (save-position-plist-merge arg
       (list
         :active (use-region-p)
         :begin begin
         :end end
-        :overlay (make-overlay begin end)))))
+        :begin-overlay begin-overlay
+        :end-overlay end-overlay))))
 
 (defun save-position-move-to-region (arg)
   (switch-to-buffer (marker-buffer (plist-get arg :begin)))
@@ -39,21 +63,19 @@
   nil)
 
 (defun save-position-apply-overlay (arg)
-  (overlay-put (plist-get arg :overlay) 'before-string
-    (propertize
-      (concat "◤" (plist-get arg :name))
-      'face `( :foreground ,(plist-get arg :fg-color)
-               :background ,(plist-get arg :bg-color))))
-  (overlay-put (plist-get arg :overlay) 'after-string
-    (propertize
-      (concat (if (plist-get arg :active) (plist-get arg :name) "") "◢")
-      'face `( :foreground ,(plist-get arg :fg-color)
-               :background ,(plist-get arg :bg-color)))))
+  (let* ( (bright-color (plist-get arg :bg-color))
+          (dim-color (color-darken-name bright-color 30))
+          (begin<end (< (marker-position (plist-get arg :begin)) (marker-position (plist-get arg :end))))
+          (begin-color (if begin<end bright-color dim-color))
+          (end-color (if begin<end dim-color bright-color)) )
+    (overlay-put (plist-get arg :begin-overlay) 'face `(:background ,begin-color))
+    (overlay-put (plist-get arg :end-overlay) 'face `(:background ,end-color))))
 
 (defun save-position-delete-region (arg)
   (if (plist-get arg :begin)
     (progn
-      (delete-overlay (plist-get arg :overlay))
+      (delete-overlay (plist-get arg :begin-overlay))
+      (delete-overlay (plist-get arg :end-overlay))
       (set-marker (plist-get arg :begin) nil)
       (set-marker (plist-get arg :end) nil)))
   (save-position-plist-merge arg
@@ -61,9 +83,10 @@
       :active nil
       :begin nil
       :end nil
-      :overlay nil)))
+      :begin-overlay nil
+      :end-overlay nil)))
 
-(defvar save-position-prev (save-position-create-new-region "p" "#fff" "#000"))
+(defvar save-position-prev (save-position-create-new-region "p" "#88c"))
 (defvar save-position-prev-enabled t)
 (defun save-position-store-prev ()
   (if (and save-position-prev-enabled (symbolp last-command))
@@ -104,14 +127,14 @@
     (define-key keymap move-to-key move-to)
     nil))
 
-(defvar save-position-1 (save-position-create-new-region "1" "#000" "#f00"))
-(defvar save-position-2 (save-position-create-new-region "2" "#000" "#00f"))
-(defvar save-position-3 (save-position-create-new-region "3" "#000" "#0f0"))
-(defvar save-position-4 (save-position-create-new-region "4" "#000" "#f0f"))
-(defvar save-position-5 (save-position-create-new-region "5" "#000" "#ff0"))
-(defvar save-position-6 (save-position-create-new-region "6" "#000" "#0ff"))
-(defvar save-position-7 (save-position-create-new-region "7" "#000" "#f80"))
-(defvar save-position-8 (save-position-create-new-region "8" "#000" "#f08"))
-(defvar save-position-9 (save-position-create-new-region "9" "#000" "#08f"))
+(defvar save-position-1 (save-position-create-new-region "1" "#c00"))
+(defvar save-position-2 (save-position-create-new-region "2" "#00c")) 
+(defvar save-position-3 (save-position-create-new-region "3" "#0c0"))
+(defvar save-position-4 (save-position-create-new-region "4" "#c0c"))
+(defvar save-position-5 (save-position-create-new-region "5" "#cc0"))
+(defvar save-position-6 (save-position-create-new-region "6" "#0cc"))
+(defvar save-position-7 (save-position-create-new-region "7" "#933"))
+(defvar save-position-8 (save-position-create-new-region "8" "#339"))
+(defvar save-position-9 (save-position-create-new-region "9" "#393"))
 
 (provide 'save-position)
